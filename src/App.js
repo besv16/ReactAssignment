d26827248e1136f6a16d31eeecb0c533846f92d6
ealborn <emma.alborn@gmail.com>
2018-04-25 08:38:52 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import WizardContainer from './Components/WizardContainer/index.js';
 import DogContainer from './Components/DogContainer/index.js';
 import './App.css';
 
-//const API_KEY = 'AIzaSyDxBpyhVYhuYUDT8vMB6YiYo_BKkxGVv6A';
 
 class App extends Component {
-  // constructor() {
-  //   super();
-  // }
   render() {
     return (
       <div className="App">
